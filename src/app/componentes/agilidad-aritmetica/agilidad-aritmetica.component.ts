6fc3b12723f5f7bfb9747ba559d1acd943a98f1b
Adrian Brizuela <adrianalebrizuela@gmail.com>
2020-09-15 00:22:39 -0300

    Juego terminado. Falta terminar estilos

--- src/app/componentes/agilidad-aritmetica/agilidad-aritmetica.component.ts
+++ src/app/componentes/agilidad-aritmetica/agilidad-aritmetica.component.ts
@@ -3,6 +3,7 @@ import { JuegoAgilidad } from '../../clases/juego-agilidad'
 
 import {Subscription} from "rxjs";
 import {TimerObservable} from "rxjs/observable/TimerObservable";
+import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-agilidad-aritmetica',
@@ -13,40 +14,104 @@ export class AgilidadAritmeticaComponent implements OnInit {
    @Output()
   enviarJuego :EventEmitter<any>= new EventEmitter<any>();
   nuevoJuego : JuegoAgilidad;
-  ocultarVerificar: boolean;
+  //ocultarVerificar: boolean;
   Tiempo: number;
   repetidor:any;
+  primerNum:number = 0;
+  segundoNum:number = 0;
+  operando:string;
+  operandos = ['+', '-', '*', '/'];
+  resultado:number;
+  resultadoReal:number;
   private subscription: Subscription;
+
   ngOnInit() {
   }
    constructor() {
-     this.ocultarVerificar=true;
-     this.Tiempo=5;
+    //this.ocultarVerificar=true;
+    this.Tiempo=5;
     this.nuevoJuego = new JuegoAgilidad();
     console.info("Inicio agilidad");
   }
-  NuevoJuego() {
-    this.ocultarVerificar=false;
-   this.repetidor = setInterval(()=>{
 
+  NuevoJuego() {
+    this.nuevoJuego = true;
+    this.ocultarVerificar();
+
+    this.primerNum = this.getRandomInt(10);
+    this.segundoNum = this.getRandomInt(10)
+    this.operando = this.operandos[this.getRandomInt(4)];
+    this.resultado = null;
+
+    switch(this.operando)
+    {
+      case '+':
+        this.resultadoReal = this.primerNum + this.segundoNum;
+        break;
+
+      case '-':
+        this.resultadoReal = this.primerNum - this.segundoNum;
+        break;
+
+      case '*':
+        this.resultadoReal = this.primerNum * this.segundoNum;
+        break;
+
+      case '/':
+      this.resultadoReal = this.primerNum / this.segundoNum;
+      break;
+    }
+
+    this.repetidor = setInterval(()=>{
       this.Tiempo--;
-      console.log("llego", this.Tiempo);
+      //console.log(this.verificar());
       if(this.Tiempo==0 ) {
         clearInterval(this.repetidor);
-        this.verificar();
-        this.ocultarVerificar=true;
+        //this.verificar();
         this.Tiempo=5;
       }
       }, 900);
-
   }
+
   verificar()
   {
-    this.ocultarVerificar=false;
     clearInterval(this.repetidor);
 
-
+    if(this.resultado == this.resultadoReal)
+    {
+      document.getElementById("btnNuevo").hidden = false;
+      document.getElementById("btnVerificar").hidden = false;
+      document.getElementById("success").hidden = false;
+      document.getElementById("fail").hidden = true;
+      this.nuevoJuego = false;
+      this.ocultarVerificar;
+      return true;
+    }
+    else
+    {
+      document.getElementById("fail").hidden = false;
+      return false;
+    }
 
   }
 
+  ocultarVerificar()
+  {
+    if(this.nuevoJuego==true)
+    {
+      document.getElementById("btnVerificar").hidden = false;
+      document.getElementById("spinJuego").hidden = false;
+      document.getElementById("success").hidden = true;
+      return false;
+    }
+    else
+    {
+      document.getElementById("btnVerificar").hidden = true;
+      return true;
+    }
+  }
+
+  getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
 }
